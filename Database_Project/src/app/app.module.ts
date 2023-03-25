9f0bc8d6ba1865929ffe54166d63af8190fcd476
EvanDyson <evandyson@ufl.edu>
2023-03-24 22:19:40 -0400

    pushing updated files, landing page, and login page

--- Database_Project/src/app/app.module.ts
+++ Database_Project/src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    RouterModule,
+    MatSlideToggleModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Database_Project/src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+    
+    hide = true;
+    loginForm: FormGroup;
+    postId: string;
+
+    constructor(private http: HttpClient, private _formBuilder: FormBuilder) { }
+    
+  ngOnInit() {
+    this.loginForm = this._formBuilder.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+    login() {
+        var formData: any = new FormData();
+        this.addData(formData);
+        this.http.post('http//localhost:1337/users/register', formData)
+            .subscribe(data => {
+                this.postId = JSON.stringify(data);
+                console.log(this.postId);
+            });
+        
+        window.location.pathname = '';
+    }   
+  
+    addData(formData: FormData) {
+        formData.append('username', this.loginForm.get('username')?.value);
+        formData.append('password', this.loginForm.get('pasword')?.value);
+    }
+}
