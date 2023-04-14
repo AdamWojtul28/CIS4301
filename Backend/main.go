b918533c06cd8c99d7308fc83e0806de35f02da6
EvanDyson <evandyson@ufl.edu>
2023-04-13 22:32:09 -0400

    Update main.go

--- Backend/main.go
+++ Backend/main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
+	LoadAppConfig()
 	// Initialize Database
 	Connect()
 	Migrate()
 
 	// host the server... keep this last
-	host := "127.0.0.1:1337"
+	host := "127.0.0.1:5000"
 	if err := http.ListenAndServe(host, httpHandler()); err != nil {
 		log.Fatalf("Failed to listen on %s: %v", host, err)
 	}
