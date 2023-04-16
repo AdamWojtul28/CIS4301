d96ab13a007ffddd6616e52fa761fb1fc35c8039
EvanDyson <evandyson@ufl.edu>
2023-04-15 23:11:33 -0400

    Delete selection-required-validator.ts

--- Frontend/src/app/datapage/selection-required-validator.ts
+++
@@ -1,4 +0,0 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
-
-export const SelectionRequiredValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null =>
-  !typeof control?.value === 'number' ? { matchRequired: true } : null;
